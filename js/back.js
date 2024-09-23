72a0eb9b69a44f53ff573db68ab62a1c6a9e4828
Alauddin-AlAzad <alaudinf92@gmail.com>
2024-09-23 22:10:59 +0600

    anta-lagano-done

---
+++ js/back.js
@@ -0,0 +1,6 @@
+document.getElementById('blog-home').addEventListener('click',function(event)
+{
+    event.preventDefault();
+    window.location.href='index.html';
+  
+});
---
+++ js/blog.js
@@ -0,0 +1,6 @@
+document.getElementById('home-blog').addEventListener('click',function(event)
+{
+    event.preventDefault();
+    window.location.href='blog.html';
+});
+//  blog-page to home page
