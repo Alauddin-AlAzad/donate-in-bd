89b4490efbcf34776e1efe088dc7dddb69fe056b
Alauddin-AlAzad <alaudinf92@gmail.com>
2024-09-24 00:18:32 +0600

    balance for noakhali

---
+++ js/donate.js
@@ -0,0 +1,25 @@
+document.getElementById('donate-now-1').addEventListener('click',function(event){
+    event.preventDefault();
+  const donateInput1=document.getElementById('input-1').value;
+   const mainBalance=document.getElementById('mainballance').innerText;
+   const mainBalanceConvert=parseFloat(mainBalance);
+  const donateInput1Convert=parseFloat(donateInput1);
+  const ballance1=document.getElementById('ballance1').innerText;
+  const balance1Convert=parseFloat(ballance1);
+
+  
+if(donateInput1Convert> 0 && mainBalanceConvert>=donateInput1Convert )
+{
+   const newBalance=mainBalanceConvert-donateInput1Convert;
+   const newBalance1=mainBalanceConvert+donateInput1Convert;
+   document.getElementById('mainballance').innerText=newBalance;
+   document.getElementById('ballance1').innerText=newBalance1;
+}
+else if(donateInput1Convert>mainBalanceConvert)
+{
+    alert("Not Enough Balance")
+}
+else{
+    alert("Please Enter Correct Value")
+}
+})
