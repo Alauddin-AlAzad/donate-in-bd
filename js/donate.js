be511b5da5ed59d00cf474936a3471d772a30d3d
Alauddin-AlAzad <alaudinf92@gmail.com>
2024-09-24 04:08:35 +0600

    setup-history-div

--- js/donate.js
+++ js/donate.js
@@ -21,14 +21,14 @@ document.getElementById('donate-now-1').addEventListener('click', function (even
     else {
         alert("Please Enter Correct Value")
     }
-    const historyItem=document.createElement("div");
+    const historyItem = document.createElement("div");
     historyItem.className = 'bg-white p-8 m-8 border-2 border-gray-300 rounded-xl ';
-    historyItem.innerHTML=`
+    historyItem.innerHTML = `
     
     <p>ppppp</p>
     `
-    const historyContainer =document.getElementById('history-section');
-    historyContainer.insertBefore(historyItem,historyContainer.firstChild);
+    const historyContainer = document.getElementById('history-section');
+    historyContainer.insertBefore(historyItem, historyContainer.firstChild);
 })
 
 // donate-2
@@ -54,19 +54,19 @@ document.getElementById('donate-now-2').addEventListener('click', function (even
     else {
         alert("Please Enter Correct Value")
     }
-    const historyItem=document.createElement("div");
+    const historyItem = document.createElement("div");
     historyItem.className = 'bg-white p-8 m-8 border-2 border-gray-300 rounded-xl ';
-    historyItem.innerHTML=`
+    historyItem.innerHTML = `
     
     <p>ppppp</p>
     `
-    const historyContainer =document.getElementById('history-section');
-    historyContainer.insertBefore(historyItem,historyContainer.firstChild);
+    const historyContainer = document.getElementById('history-section');
+    historyContainer.insertBefore(historyItem, historyContainer.firstChild);
 })
 
 // donate 3
 document.getElementById('donate-now-3').addEventListener('click', function (event) {
-event.preventDefault();
+    event.preventDefault();
     const donateInput1 = document.getElementById('input-3').value;
     const mainBalance = document.getElementById('mainballance').innerText;
     const mainBalanceConvert = parseFloat(mainBalance);
@@ -87,28 +87,30 @@ event.preventDefault();
     else {
         alert("Please Enter Correct Value")
     }
-    const historyItem=document.createElement("div");
+    const historyItem = document.createElement("div");
     historyItem.className = 'bg-white p-8 m-8 border-2 border-gray-300 rounded-xl ';
-    historyItem.innerHTML=`
+    historyItem.innerHTML = `
     
     <p>ppppp</p>
     `
-    const historyContainer =document.getElementById('history-section');
-    historyContainer.insertBefore(historyItem,historyContainer.firstChild);
-    
+    const historyContainer = document.getElementById('history-section');
+    historyContainer.insertBefore(historyItem, historyContainer.firstChild);
+
 })
+// button-history
 
-
-document.getElementById('history').addEventListener('click', function() {
-    this.classList.add('bg-primary-btn');
+const historyBtn = document.getElementById('history');
+historyBtn.addEventListener('click', function () {
+    historyBtn.classList.add('bg-primary-btn');
     document.getElementById('blog').classList.remove('bg-primary-btn');
     document.getElementById('main-tab').classList.add('hidden');
     document.getElementById('history-section').classList.remove('hidden');
 });
 
 
-document.getElementById('blog').addEventListener('click', function() {
-    this.classList.add('bg-primary-btn');
+const blogBtn = document.getElementById('blog');
+  blogBtn.addEventListener('click', function () {
+    blogBtn.classList.add('bg-primary-btn');
     document.getElementById('history').classList.remove('bg-primary-btn');
     document.getElementById('main-tab').classList.remove('hidden');
     document.getElementById('history-section').classList.add('hidden');
